ed2801f2ab741a0a29555ca60e70542d009f82e7
John Doe <raijatin2006@gmail.com>
2026-01-10 00:07:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const apiKey="45c13e12b23f1b0e8a53d28d3f1e4f37"
+const apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const searchBox= document.querySelector(".search-box input")
+const searchBtn= document.querySelector(".search-box button")
+const weatherIcon=document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    const response=await fetch(apiURL+city+`&appid=${apiKey}`)
+
+    if(response.status==404){
+    document.querySelector(".error").style.display="block"
+    document.querySelector(".weather").style.display="none"
+}
+else{
+    document.querySelector(".error").style.display="none"
+    document.querySelector(".weather").style.display="block"
+    
+    }
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+" km/h";
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="images/clouds.png"
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src="images/rain.png"
+    }
+    else if(data.weather[0].main=="Snow"){
+        weatherIcon.src="images/snow.png"
+    }
+    else if(data.weather[0].main=="Clear"){
+        weatherIcon.src="images/clear.png"
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src="images/mist.png"
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src="images/drizzle.png"
+    }
+    document.querySelector(".weather").style.display="block"
+    document.querySelector(".error").style.display="none"
+}
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+})
+searchBox.addEventListener("keydown",function(e){
+    if(e.key==="Enter"){
+        searchBtn.click();
+    }
+})
+
+
